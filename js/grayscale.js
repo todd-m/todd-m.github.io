7dbc0d7e94ad00d835ab4a95e0b5f68901b5da5c
Jenn Dodd and Mike McDearmon <pair+jedodd+mmcdearmon@pivotallabs.com>
2015-07-18 19:01:37 -0400

    added album art background, updated news link, adjusted css slideshow for single image, and adjust page scrolling to be less annoying

--- js/grayscale.js
+++ js/grayscale.js
@@ -19,7 +19,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1100, 'easeInOutExpo');
+        }, 1100, 'easeOutExpo');
         event.preventDefault();
     });
 });

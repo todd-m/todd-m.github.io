e31df28760899e475da94617ce01294965d7e582
Mike McDearmon <pair+mmcdearmon@pivotallabs.com>
2016-04-24 09:34:36 -0400

    add spotify listen and follow

--- js/grayscale.js
+++ js/grayscale.js
@@ -72,6 +72,20 @@ $(document).ready(function () {
     });
 });
 
+// Responsive Spotify embed widget
+
+$(window).on('load resize', function() {
+  $('iframe[src*="embed.spotify.com"]').each( function() {
+    $(this).css('width', $(this).parent().css('width'));
+    $(this).attr('src', $(this).attr('src'));
+    $(this).removeClass('loaded');
+    
+    $(this).on('load', function(){
+      $(this).addClass('loaded');
+    });
+  });
+});
+
 
 // Current Date
 var d = new Date();

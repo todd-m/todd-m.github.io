dc3a5f22d65d12c0bdcc75a4ecbeabe453439f5f
Mike McDearmon <mmcdearmon@gmail.com>
2018-02-19 22:20:51 -0500

    collapsed navbar hides after click

--- js/grayscale.js
+++ js/grayscale.js
@@ -4,14 +4,11 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+
+$(".page-scroll").click(function(){
+  $(".navbar-collapse").removeClass("in");  
+})
+  
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
